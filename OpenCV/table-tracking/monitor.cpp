ab7c2cc5d18f2e8070751c3aec8ce2378288d586
Edwin Gonzalez <gonzalez.edwin2311@gmail.com>
2020-01-05 18:27:06 -0800

    Added Red color recognition

--- OpenCV/table-tracking/monitor.cpp
+++ OpenCV/table-tracking/monitor.cpp
@@ -38,7 +38,7 @@ using namespace Json;
 void display(Mat image);
 
 int main () {
-
+/*
     VideoCapture capture;
     capture.open(0);
     if ( ! capture.isOpened() )
@@ -63,11 +63,26 @@ int main () {
             break; // escape
         }
     }
+*/
+    Mat image;
+    image = imread("images/colors2.png"); // Read the file
+    resize(image,image,Size(250,250), 0, 0, INTER_LINEAR_EXACT);
+    if( image.empty() )                      // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    display(image);
+    waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
 
 void display(Mat image) {
-    imshow("Camera Window", image);
+    Mat hsv;
+    cvtColor(image,hsv,COLOR_BGR2HSV);
+    Mat red;
+    inRange(image,Scalar(0,0,0),Scalar(0,0,255), red);
+    imshow("Camera Window", red);
 }
 
 
